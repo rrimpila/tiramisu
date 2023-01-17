942680b4bdf40dda253643abba1291c1544f3298
Minerva Ciccarese <minervaciccarese@outlook.com>
2023-01-17 13:42:34 +0200

    #3 added to calculation and changed commit message

--- test_code.py
+++ test_code.py
@@ -9,8 +9,7 @@ print("There once was a man from Nantucket Who kept all his cash in a bucket.")
 print("But his daughter, named Nan, Ran away with a man")
 print("And as for the bucket, Nantucket.")
 
-calculation = 1 + 1 # issue #3 resolved
-
+calculation = 1 + 1 +1 # issue #3 resolved
 print(calculation)
 
 
