f454fd9bfc4c0f06afdae4267782476f235d1a13
Tiia Poikajärvi <tiia.poikajarvi@helsinki.fi>
2023-01-17 13:27:21 +0200

    Issue #1 resolved, comments added to the file

--- test_code.py
+++ test_code.py
@@ -1,6 +1,12 @@
-print("Goodbye World!")
+# This is a Tiramisu test python file to pull and push changes to Github repository:
+
+print("Goodbye World!") # issue #2 resolved
 print("Hello Tiia and Minerva!")
 print("Hewwo OwO")
 
-calculation = 1 + 1
+calculation = 1 + 1 # issue #3 resolved
 print(calculation)
+
+
+# comments added, issue #1 resolved
+
