2ba53cc8f360a7aee523057d8ba66528a1f0a000
Rimpilä Raine J <rimpila@vdi-cubic-012.ad.helsinki.fi>
2023-01-17 13:22:11 +0200

    #4 Added limerick

--- test_code.py
+++ test_code.py
@@ -2,5 +2,10 @@ print("Goodbye World!")
 print("Hello Tiia and Minerva!")
 print("Hewwo OwO")
 
+# Limerick for your enjoyment
+print("There once was a man from Nantucket Who kept all his cash in a bucket.")
+print("But his daughter, named Nan, Ran away with a man")
+print("And as for the bucket, Nantucket.")
+
 calculation = 1 + 1
 print(calculation)
